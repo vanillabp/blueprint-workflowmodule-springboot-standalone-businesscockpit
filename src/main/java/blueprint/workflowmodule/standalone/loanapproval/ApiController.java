a700682c1fbffbbf7a439c0dc75c087696d4dae2
Stephan Pelikan <stephan.pelikan@phactum.at>
2026-08-06 14:30:41 +0200

    Include BPMN execution history

--- src/main/java/blueprint/workflowmodule/standalone/loanapproval/ApiController.java
+++ src/main/java/blueprint/workflowmodule/standalone/loanapproval/ApiController.java
@@ -1,11 +1,13 @@
 package blueprint.workflowmodule.standalone.loanapproval;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 import blueprint.workflowmodule.standalone.loanapproval.config.LoanApprovalProperties;
 import blueprint.workflowmodule.standalone.loanapproval.user.BlueprintUserService;
 import io.vanillabp.spi.cockpit.usertask.UserTask;
+import io.vanillabp.spi.process.ProcessDefinition;
+import io.vanillabp.spi.process.WorkflowHistory;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -221,6 +225,32 @@ public class ApiController {
 
         log.info("Fetched case: {} with data: {}", loanRequestId, response);
         return ResponseEntity.ok(response);
+
+    }
+
+    @GetMapping("/{loanRequestId}/process-definition")
+    public ResponseEntity<List<ProcessDefinition>> getProcessDefinitions(
+            @PathVariable final String loanRequestId) {
+
+        return ResponseEntity.ok(service.getProcessDefinition(loanRequestId));
+
+    }
+
+    @GetMapping("/bpmn/{processDefinitionId}")
+    public ResponseEntity<InputStreamResource> getBpmnXml(
+            @PathVariable final String processDefinitionId) {
+
+        final var xml = service.getProcessDiagram(processDefinitionId);
+        return ResponseEntity.ok(new InputStreamResource(xml));
+
+    }
+
+    @GetMapping("/{loanRequestId}/workflow-history")
+    public ResponseEntity<WorkflowHistory> getWorkflowHistory(
+            @PathVariable final String loanRequestId,
+            @RequestParam(required = false) final String historyContext) {
+
+        return ResponseEntity.ok(service.getWorkflowHistory(loanRequestId, historyContext));
 
     }
 
